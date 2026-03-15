9264f6b02050b89ff6c95427ccf602c2dfd53813
MMS139 <asumshakeel@gmail.com>
2026-03-15 16:26:00 +1100

    Low Gravity

--- levels/level2.gd
+++ levels/level2.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
 @export var player_scene : PackedScene
+var player_instance : Node = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spawn_player()
+	player_instance.gravity = 400
 
 func spawn_player():
-	var player = player_scene.instantiate()
-	player.position = $SpawnPoint.position
-	add_child(player)
+	player_instance = player_scene.instantiate()
+	player_instance.position = $SpawnPoint.position
+	add_child(player_instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
